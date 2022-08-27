1b42eedfbafb9e1cbfb1d853c2de0f10ccb5e5e0
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-08-27 11:58:36 -0700

    adding folders functionality

--- src/website.js
+++ src/website.js
@@ -1,6 +1,7 @@
 import {fetchTodo} from "./todos";
 
-const taskInfo = ['title', 'description', 'due-date', 'priority'];
+const taskInfo = ['title', 'description', 'due-date', 'priority', 'folder'];
+let folders = ['main'];
 
 function createTodoForm() {
     // a div to collect todo info (left side of screen)
@@ -11,13 +12,32 @@ function createTodoForm() {
 
     taskInfo.forEach(element => {
         const itemLabel = document.createElement('label');
-        const itemInput = document.createElement('input');
-
-        itemInput.id = element;
-        itemInput.type = 'text';
-
         itemLabel.for = element;
         itemLabel.textContent = element.toUpperCase();
+
+        let itemInput;
+        let selectedFolder;
+
+        if(element === 'folder') {
+            itemInput = document.createElement('select');
+            createDropdown(itemInput, selectedFolder='main');
+
+            const newFolder = document.createElement('button');
+            newFolder.type = 'button';
+            newFolder.textContent = '+ new folder';
+            itemLabel.appendChild(newFolder);
+            newFolder.addEventListener('click', () => {
+                folders.push(prompt('What is the folder name?'));
+                createDropdown(itemInput, selectedFolder = folders[folders.length-1]);
+            });
+
+        }
+        else {
+            itemInput = document.createElement('input');
+    
+            itemInput.id = element;
+            itemInput.type = 'text';
+        }
 
         todoForm.append(itemLabel, itemInput);
     })
@@ -54,4 +74,19 @@ function createTodoDisplay() {
     return displayingDiv;
 }
 
+function createDropdown(dropdown, selectedFolder) {
+    while (dropdown.firstChild) {
+        dropdown.removeChild(dropdown.firstChild);
+    }
+    folders.forEach(folder => {
+        const option = document.createElement('option');
+        
+        option.textContent = folder;
+        option.value = folder;
+
+        if(folder === selectedFolder) option.selected = true; 
+
+        dropdown.appendChild(option);
+    });
+}
 export {createTodoForm, createTodoDisplay};
